95a36049ce5868e919e3bd582dc5d3b45ea71d74
khoi <khoitay11566@email.com>
2025-11-01 19:21:43 +0700

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -58,7 +58,7 @@ const queryClient = new QueryClient({
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <div lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
@@ -87,6 +87,6 @@ export default function RootLayout({ children }) {
           </WagmiProvider>
         </ReduxProvider>
       </body>
-    </html>
+    </div>
   );
 }
